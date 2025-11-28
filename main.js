a61280bb5ce489b8f43e51f5c117d261e0aa7543
burlukkara24 <burlukkara24@itu.edu.tr>
2025-11-28 21:16:34 +0300

    feat: Blockchain sinifi ve Genesis blok yapisi eklendi

--- main.js
+++ main.js
@@ -25,3 +25,34 @@ class Block {
         console.log("Bloğunuz Kazıldı:" + this.hash);
     }
 }
+// --- 2. ADIM: ZİNCİR (BLOCKCHAIN) YAPISI ---
+class Blockchain {
+    constructor() {
+        // Tren raylara indiğinde ilk vagonu (Genesis) otomatik ekle
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2; // Zorluk seviyesi (Mining için)
+    }
+
+    // İlk vagonu (Atayı) yaratan fonksiyon
+    createGenesisBlock() {
+        return new Block(1, "28/11/2025", "Genesis Block", "0");
+    }
+
+    // Zincirin en sonundaki vagonu bulur
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    // Zincire yeni vagon ekler
+    addBlock(newBlock) {
+        // 1. Yeni gelen vagonun "prevHash"ine, şu anki son vagonun hash'ini yazar
+        newBlock.prevHash = this.getLatestBlock().hash;
+
+        // 2. Madencilik (Mining) işlemini başlatır
+        // (Zorluk seviyesine göre hash bulmaya çalışır)
+        newBlock.mineBlock(this.difficulty);
+
+        // 3. Vagonu zincire ekler
+        this.chain.push(newBlock);
+    }
+}
